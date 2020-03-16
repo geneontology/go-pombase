21c187a06892f423ad53956f2910c652fce01e9d
dustine32 <dustine32@gmail.com>
2020-03-16 12:23:07 -0700

    X filter param and cluster sorting by geneset size

--- pombase_direct_bp_annots_query.py
+++ pombase_direct_bp_annots_query.py
@@ -43,6 +43,12 @@ class GOTermAnalyzer():
         # return subont.parents(go_term, relations=["subClassOf","BFO:0000050"])
         return subont.parents(go_term, relations=["subClassOf"])
 
+    def get_children(self, go_term):
+        all_children = self.onto.children(go_term)
+        all_children.append(go_term)
+        subont = self.onto.subontology(all_children)
+        return subont.children(go_term, relations=["subClassOf"])
+
     def is_biological_process(self, go_term):
         bp_root = "GO:0008150"
         if go_term == bp_root:
@@ -144,19 +150,29 @@ class TermAnnotationDictionary():
                 return True
         return False
 
-    def select_candidate_bps(self, n=30, m=10):
+    def select_candidate_bps(self, n, m, x):
         if n is None:
             n = 30
         if m is None:
             m = 5
+        if x is None:
+            x = 60
         print("n =", n)
         print("m =", m)
+        print("x =", x)
         candidate_bps = {}
         for bp in self.bps:
-            if len(self.bps[bp]) >= m and len(self.bps[bp]) <= n and self.has_parent_with_direct_annotations_greater_than(bp, n):
+            gene_set_size = len(self.bps[bp])
+            if gene_set_size > x:
+                # Filter out BP if geneset's too big (>x)
+                continue
+            bp_children = self.analyzer.get_children(bp)
+            if (gene_set_size >= m and gene_set_size <= n and self.has_parent_with_direct_annotations_greater_than(bp, n))\
+                    or (gene_set_size > n and len(bp_children) == 0):
                 candidate_bps[bp] = self.bps[bp]
         candidate_bp_keys = list(candidate_bps.keys())
         for bp in candidate_bp_keys:
+            # Only get most generic terms of candidates - remove term from candidates if an ancestor also candidate
             if len(set(self.analyzer.get_ancestors(bp)) & set(candidate_bp_keys)) > 0:
                 del candidate_bps[bp]
         return candidate_bps
@@ -167,8 +183,8 @@ class TermAnnotationDictionary():
             new_list[t] = self.bps[t]
         return new_list
 
-    def get_our_nice_lists(self, n=30, m=10):
-        self.candidate_bps = self.select_candidate_bps(n, m)
+    def get_our_nice_lists(self, n=30, m=10, x=60):
+        self.candidate_bps = self.select_candidate_bps(n, m, x)
         self.pair_list = self.grouper.pair_bp_sets_with_similar_genes(m)
         self.cluster_list = self.grouper.cluster_pair_list(self.pair_list)
         self.unpaired_bps = self.grouper.find_unpaired_bps(self.pair_list)
@@ -224,18 +240,25 @@ class BPTermSimilarityGrouper():
             for bpy in bp_dict:
                 if bpx != bpy and (bpx, bpy) not in set_list:
                     common_genes_between_x_and_y = set(bp_dict[bpx]) & set(bp_dict[bpy])
-                    if len(common_genes_between_x_and_y) >= min(m, len(bp_dict[bpx]), len(bp_dict[bpy])):
-                        set_list.append((bpx, bpy))
+                    num_of_common_genes = len(common_genes_between_x_and_y)
+                    if num_of_common_genes >= min(m, len(bp_dict[bpx]), len(bp_dict[bpy])):
+                        # Add weight (num_of_common_genes) to these pairs
+                        set_list.append((bpx, bpy, num_of_common_genes))
             progress.print_progress()
         return set_list
 
     def cluster_pair_list(self, result_sets):
         clusters = []
+        used_terms = set()  # Track used_terms
+        # Sort result_sets (pairs) by # of common genes?
+        result_sets = sorted(result_sets, key=lambda x: x[2], reverse=True)
         for r_set in result_sets:
             c_index = 0
             cluster_found = False
             term1 = r_set[0]
             term2 = r_set[1]
+            if term1 in used_terms and term2 in used_terms:
+                continue
             for cluster in clusters:
                 if term1 in cluster and term2 in cluster:
                     cluster_found = True
@@ -253,19 +276,21 @@ class BPTermSimilarityGrouper():
                 c_index += 1
             if not cluster_found:
                 clusters.append([term1, term2])
+            used_terms.add(term1), used_terms.add(term2)
         return clusters
 
     def uniqueify_tuple_terms(self, tuples):
         unique_items = [] 
-        for a, b in tuples:
+        for a, b, weight in tuples:
             if a not in unique_items:
                 unique_items.append(a)
             if b not in unique_items:
                 unique_items.append(b)
         return unique_items
 
-    def find_unpaired_bps(self, plist):
-        bp_dict = self.tad.bps
+    def find_unpaired_bps(self, plist, bp_dict=None):
+        if bp_dict is None:
+            bp_dict = self.tad.bps
         unpaired_bp_list = []
         unique_bps = self.uniqueify_tuple_terms(plist)
         for bp in bp_dict:
@@ -274,28 +299,40 @@ class BPTermSimilarityGrouper():
         return unpaired_bp_list
 
     @staticmethod
-    def format_cluster_header(cluster_counter, cluster):
-        header_print_fmt = "---- Cluster {cluster_counter}, Total: {cluster_length} ----"
-        return header_print_fmt.format(cluster_counter=str(cluster_counter), cluster_length=str(len(cluster)))
+    def format_cluster_header(cluster_counter, cluster, gene_count):
+        header_print_fmt = "---- Cluster {cluster_counter}, Total terms: {cluster_length}, Total genes: {gene_count} ----"
+        return header_print_fmt.format(cluster_counter=str(cluster_counter), cluster_length=str(len(cluster)), gene_count=gene_count)
 
-    def format_cluster_line(self, term, bp_genes):
+    def format_cluster_line(self, term, gene_count):
         line_print_fmt = "{term} - {term_label} - {gene_count}"
-        return line_print_fmt.format(term=term, term_label=self.analyzer.label(term), gene_count=len(bp_genes[term]))
+        return line_print_fmt.format(term=term, term_label=self.analyzer.label(term), gene_count=gene_count)
 
     def print_clusters(self, clusters, bp_list, c_out=None):
         cluster_counter = 1
         if c_out is None:
             for c in clusters:
-                print(self.format_cluster_header(cluster_counter, c))
+                lines = []
+                genes = set()
                 for t in c:
-                    print(self.format_cluster_line(t, bp_list))
+                    term_genes = set(bp_list[t])
+                    genes = genes | term_genes
+                    lines.append(self.format_cluster_line(t, len(term_genes)))
+                header = self.format_cluster_header(cluster_counter, c, len(genes))
+                print(header)
+                [print(l) for l in lines]
                 cluster_counter += 1
         else:
             with open(c_out, 'w') as f:
                 for c in clusters:
-                    f.write(self.format_cluster_header(cluster_counter, c) + "\n")
+                    lines = []
+                    genes = set()
                     for t in c:
-                        f.write(self.format_cluster_line(t, bp_list) + "\n")
+                        term_genes = set(bp_list[t])
+                        genes = genes | term_genes
+                        lines.append(self.format_cluster_line(t, len(term_genes)) + "\n")
+                    header = self.format_cluster_header(cluster_counter, c, len(genes)) + "\n"
+                    f.write(header)
+                    [f.write(l) for l in lines]
                     cluster_counter += 1
 
     def append_singletons_to_outfile(self, singletons, cluster_outfile=None):
@@ -303,10 +340,10 @@ class BPTermSimilarityGrouper():
             return
         with open(cluster_outfile, "a") as c_out:
             c_out.write("---- Singleton Clusters ----\n")
-            # TODO: sort keys by len(singletons[t])
+            # Sort keys by gene list size descending
             singleton_terms = sorted(singletons.keys(), key=lambda x: len(singletons[x]), reverse=True)
             for t in singleton_terms:
-                c_out.write(self.format_cluster_line(t, singletons) + "\n")
+                c_out.write(self.format_cluster_line(t, len(singletons[t])) + "\n")
 
 
 class ProgressTracker:
@@ -333,23 +370,20 @@ class ProgressTracker:
         return datetime.datetime.now() - self.start
 
 def create_association_set(filename, ontology):
+    # Filter out non-exp
     gas = GafAnnotationSet(filename, ontology=ontology, filter_evidence=True)
     a_set = gas.association_set
     return a_set
 
-def do_everything(n=30, m=10, outfile=None, c_out=None, s_out=None, gaf_file=None, reuse_tad_json=None):
+def do_everything(n, m, x, outfile=None, c_out=None, s_out=None, gaf_file=None, reuse_tad_json=None):
     # onto, aset = setup_pombase()
     ontology = OntologyFactory().create("go")
-    # afactory = AssociationSetFactory()
-    # TODO: filter out non-exp
-    # association_set = afactory.create(ontology, "gene", "function", taxon=POMBASE)
-    # association_set = afactory.create(ontology, "gene", "function", file=gaf_file)
     association_set = create_association_set(gaf_file, ontology)
     print("aset size:", len(association_set.association_map))
     tad = TermAnnotationDictionary(ontology, association_set, json_file=reuse_tad_json)
     tad.print_results(outfile)
     print("Initial BP count: " + str(len(tad.bps)))
-    new_bps = tad.select_candidate_bps(n, m)
+    new_bps = tad.select_candidate_bps(n, m, x)
     print("Candidate BP count: " + str(len(new_bps)))
     grouper = BPTermSimilarityGrouper(tad)
     p_list = grouper.pair_bp_sets_with_similar_genes(new_bps, m)
@@ -358,7 +392,7 @@ def do_everything(n=30, m=10, outfile=None, c_out=None, s_out=None, gaf_file=Non
     distinct_paired_bp_terms = grouper.uniqueify_tuple_terms(p_list)
     print("Paired BP (distinct) count:", len(distinct_paired_bp_terms))
 
-    unpaired_bps = grouper.find_unpaired_bps(p_list)
+    unpaired_bps = grouper.find_unpaired_bps(p_list, bp_dict=new_bps)
     unpaired_bps_gene_lists = tad.term_subset(unpaired_bps)
     tad.print_results(s_out, unpaired_bps_gene_lists)
     print("Unpaired BP count:", len(unpaired_bps_gene_lists))
@@ -385,9 +419,9 @@ def main():
                         help="File name of BP terms that did not cluster (singletons)")
     parser.add_argument('-n', "--n_value", type=str, required=False,
                         help="1. Get all the BP terms X, where the number of genes annotated to X are less than or equal to n, and the number of genes annotated to a (is_a or part_of) parent of X are greater than n")
-    # TODO: Make sure i'm excluding descendants of picked terms
     parser.add_argument('-m', "--m_value", type=str, required=False,
                         help="2. Of the BPs from step 1, cluster BPs X and Y together if the number of genes in common between them is greater than or equal to min(m, number of genes in X, number of genes in Y)")
+    parser.add_argument('-x', "--x_value", type=int)
     parser.add_argument('-g', "--gaf_source", type=str, required=True,
                         help="filename of GAF file to use as annotation source")
     parser.add_argument('-j', "--dump_tad_json", type=str, required=False,
@@ -407,7 +441,7 @@ def main():
         print("JSON created")
     else:
         print("Getting your lists for you...")
-        do_everything(args.n_value, args.m_value, args.term_gene_count_outfile, args.clusters_outfile, args.unclustered_outfile, gaf_file=args.gaf_source, reuse_tad_json=args.reuse_tad_json)
+        do_everything(n=args.n_value, m=args.m_value, x=args.x_value, outfile=args.term_gene_count_outfile, c_out=args.clusters_outfile, s_out=args.unclustered_outfile, gaf_file=args.gaf_source, reuse_tad_json=args.reuse_tad_json)
 
 if __name__ == "__main__":
     main()
